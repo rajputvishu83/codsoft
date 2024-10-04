ea228a5cd19b932eb72b8f794af011347fa84c44
rajputvishu83 <chouhanvishu97@gmail.com>
2024-10-04 09:55:39 +0530

    Add files via upload

---
+++ task 2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class StudentGrades {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+
+        // Array to store marks for each subject
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        // Input marks for each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            totalMarks += marks[i]; // Calculate total marks
+        }
+
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        // Determine the grade based on average percentage
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display the results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
